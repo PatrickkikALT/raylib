801d6ad30330ac0e9011b44f0b2c637f0949ef66
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2025-07-26 20:53:57 +0200

    Add basic player movement and gravity

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ const int MAX_HEIGHT = 20;
 const int OCTAVES = 2;
 const float NOISE_SCALE = 0.05f;
 const float BLOCK_SIZE = 1.0f;
+const float GRAVITY = 9.8f;
 
 bool generatedTerrain = false;
 
@@ -112,17 +113,126 @@ int main() {
   camera.fovy = 45.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
+  float yaw = 0;
+  float pitch = 0;
+
   if (!generatedTerrain) {
     GenerateTerrain();
   }
+  bool grounded = false;
+  float playerVelocity = 0.0f;
 
   while (!WindowShouldClose()) {
-    UpdateCamera(&camera, CAMERA_FREE);
-
     BeginDrawing();
     ClearBackground(SKYBLUE);
     DrawFPS(10, 30);
     BeginMode3D(camera);
+
+    float deltaTime = GetFrameTime();
+    
+    playerVelocity -= GRAVITY * deltaTime;
+
+    if (grounded && IsKeyPressed(KEY_SPACE)) {
+      playerVelocity = -6.0f;
+      grounded = false;
+    }
+
+    Vector3 nextPos = camera.position;
+    nextPos.y -= playerVelocity * deltaTime;
+
+    int nx = (int)nextPos.x;
+    int ny = (int)(nextPos.y - 1.0f);
+    int nz = (int)nextPos.z;
+
+    if (nx >= 0 && nx < WORLD_SIZE && ny >= 0 && ny < MAX_HEIGHT && nz >= 0 && nz < WORLD_SIZE) {
+      if (!solid[nx][ny][nz]) {
+        camera.position.y = nextPos.y;
+        grounded = false;
+      }
+      else {
+        grounded = true;
+        playerVelocity = 0;
+        camera.position.y = ny + 1.0f;
+      }
+    }
+
+
+    //ground check
+    Ray ray = { camera.position, { 0.0f, 1.0f, 0.0f} };
+    ray.direction = Vector3{ 0.0f, -1.0f, 0.0f };
+
+    float closestDist = FLT_MAX;
+    Vector3 groundPos = { 0 };
+
+    //bad for performance
+    //for (Block& block : blocks) {
+    //  RayCollision hit = GetRayCollisionMesh(ray, cubeMesh, MatrixTranslate(block.position.x, block.position.y, block.position.z));
+    //  if (hit.hit && hit.distance < closestDist) {
+    //    closestDist = hit.distance;
+    //    groundPos = hit.point;
+    //  }
+    //}
+    Vector3 camPos = camera.position;
+    int x = (int)camPos.x;
+    int z = (int)camPos.z;
+
+    for (int y = (int)camPos.y; y >= 0; y--) {
+      if (x >= 0 && x < WORLD_SIZE && y < MAX_HEIGHT && z >= 0 && z < WORLD_SIZE) {
+        if (solid[x][y][z]) {
+          groundPos.y = y + 1.4f;
+          break;
+        }
+      }
+    }
+
+    camera.position.y += playerVelocity * deltaTime;
+    //apply gravity
+    if (camera.position.y >= groundPos.y + 1.4f) {
+      camera.position.y -= GRAVITY * deltaTime;
+    }
+
+    //mouse movement
+
+    Vector2 delta = GetMouseDelta();
+    yaw -= delta.x * 0.002f;
+    pitch -= delta.y * 0.002f;
+    if (pitch > 89.0f * DEG2RAD) pitch = 89.0f * DEG2RAD;
+    if (pitch < -89.0f * DEG2RAD) pitch = -89.0f * DEG2RAD;
+
+    //movement
+    Vector3 forward = {
+      cosf(pitch) * sinf(yaw),
+      sinf(pitch),
+      cosf(pitch) * cosf(yaw)
+    };
+    Vector3 right = {
+      sinf(yaw - PI / 2.0f),
+      0.0f,
+      cosf(yaw - PI / 2.0f)
+    };
+
+    Vector3 up = Vector3CrossProduct(right, forward);
+
+    Vector3 oldPosition = camera.position;
+
+    if (IsKeyDown(KEY_W)) camera.position = Vector3Add(camera.position, Vector3Scale(forward, 10 * deltaTime));
+    if (IsKeyDown(KEY_S)) camera.position = Vector3Subtract(camera.position, Vector3Scale(forward, 10 * deltaTime));
+    if (IsKeyDown(KEY_A)) camera.position = Vector3Subtract(camera.position, Vector3Scale(right, 10 * deltaTime));
+    if (IsKeyDown(KEY_D)) camera.position = Vector3Add(camera.position, Vector3Scale(right, 10 * deltaTime));
+
+
+
+    Vector3 testPos = camera.position;
+    int tx = (int)testPos.x;
+    int ty = (int)(testPos.y - 1.0f);
+    int tz = (int)testPos.z;
+    if (tx >= 0 && tx < WORLD_SIZE && ty >= 0 && ty < MAX_HEIGHT && tz >= 0 && tz < WORLD_SIZE) {
+      if (solid[tx][ty][tz]) {
+        camera.position = oldPosition;
+      }
+    }
+    camera.target = Vector3Add(camera.position, forward);
+    camera.up = up;
 
     for (const Block& block : blocks) {
       if (ColorIsEqual(block.color, GREEN)) {
@@ -156,7 +266,6 @@ int main() {
           
         }
       }
-      printf("%d\n", selected);
       if (selected >= 0) {
         Vector3 pos = blocks[selected].position;
         solid[pos.x][pos.y][pos.z] = false;
@@ -164,8 +273,6 @@ int main() {
       }
     }
     
-
-
     EndMode3D();
     DrawText("WASD + mouse to move", 10, 10, 20, BLACK);
     EndDrawing();
